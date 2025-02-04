9c1ab66f2e71ad72040496a639f891167141387c
Alexandt <dgv17022005@gmail.com>
2025-02-04 19:06:17 +0500

    update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import datetime
 import pandas
 import collections
 
-excel_data_wine = pandas.read_excel('wine3.xlsx', na_values=['N/A', 'NA'], keep_default_na=False)
-excel_data_wine_dict = excel_data_wine.to_dict(orient='records')
+excel_wine_data = pandas.read_excel('wine3.xlsx', na_values=['N/A', 'NA'], keep_default_na=False)
+excel_data_wine_dict = excel_wine_data.to_dict(orient='records')
 wine_dict = collections.defaultdict(list)
 
 for wine in excel_data_wine_dict:
