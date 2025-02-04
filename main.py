0bac6e99d8ed373ad673f9e84bc42d0fd6859d9e
Alexandt <dgv17022005@gmail.com>
2025-02-04 19:36:06 +0500

    update main.py

--- main.py
+++ main.py
@@ -1,36 +1,9 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime
 import pandas
 import collections
 import argparse
-
-
-parser = argparse.ArgumentParser(description='Путь к файлу')
-parser.add_argument('url', help='Ваш файл')
-args = parser.parse_args()
-
-user_input = args.url
-
-
-excel_drinks_data = pandas.read_excel(user_input, na_values=['N/A', 'NA'], keep_default_na=False)
-drinks_data = excel_drinks_data.to_dict(orient='records')
-drinks_collection = collections.defaultdict(list)
-
-for wine in drinks_data:
-    drinks_collection[wine['Категория']].append(wine)
-
-type_drink = []
-
-for drink in drinks_collection:
-    type_drink.append(drink)
-
-
-start_date = datetime.datetime(year=1920, month=1, day=1)
-end_date = datetime.datetime(year=2023, month=1, day=1)
-day_in_year = 365
-delta = end_date-start_date
-final_year = int(delta.days/day_in_year)
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
 def check_year(year):
@@ -48,22 +21,52 @@ def check_year(year):
         return "лет"
 
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+def main():
 
-template = env.get_template('template.html')
+    parser = argparse.ArgumentParser(description='Путь к файлу')
+    parser.add_argument('url', help='Ваш файл')
+    args = parser.parse_args()
 
-rendered_page = template.render(
-    year_title=f'{final_year} {check_year(final_year)}',
-    white_wines=drinks_collection[type_drink[0]],
-    drinks=drinks_collection[type_drink[1]],
-    red_wines=drinks_collection[type_drink[2]]
-)
+    user_input = args.url
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    excel_drinks_data = pandas.read_excel(user_input, na_values=['N/A', 'NA'], keep_default_na=False)
+    drinks_data = excel_drinks_data.to_dict(orient='records')
+    drinks_collection = collections.defaultdict(list)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    for wine in drinks_data:
+        drinks_collection[wine['Категория']].append(wine)
+
+    type_drink = []
+
+    for drink in drinks_collection:
+        type_drink.append(drink)
+
+    start_date = datetime.datetime(year=1920, month=1, day=1)
+    end_date = datetime.datetime(year=2023, month=1, day=1)
+    day_in_year = 365
+    delta = end_date-start_date
+    final_year = int(delta.days/day_in_year)
+
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+
+    template = env.get_template('template.html')
+
+    rendered_page = template.render(
+        year_title=f'{final_year} {check_year(final_year)}',
+        white_wines=drinks_collection[type_drink[0]],
+        drinks=drinks_collection[type_drink[1]],
+        red_wines=drinks_collection[type_drink[2]]
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+if __name__ == "__main__":
+    main()
